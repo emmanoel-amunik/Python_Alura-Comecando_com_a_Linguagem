4d6463fd019d5aa4259e53af5cbfb1430aaee5f0
Emmanoel <emmanoelamunik@hotmail.com>
2023-11-24 06:33:12 -0300

    New conditions and optimized

--- Python_Jogos/adivinhacao.py
+++ Python_Jogos/adivinhacao.py
@@ -1,14 +1,27 @@
-print("*****************************")
+print("*" * 29)
 print("Welcome to the Guessing game!")
-print("*****************************")
+print("*" * 29)
 
-secret_number = 42
+secret_number = 85
 user_kick = int(input("Type a number: "))
 
+# user_kick = int(user_kick)  \\ teacher's solution
 print("You entered: ", user_kick)
 
-# user_kick = int(user_kick)  \\ teacher's solution
-if secret_number == user_kick:
+
+right = user_kick == secret_number
+greater = user_kick > secret_number
+less = user_kick < secret_number
+
+
+if right:
     print("You're right!!!")
 else:
-    print("You're wrong. Try again!")
+    if greater:
+        print("You're wrong. Your guess was greater than the secret number. ")
+    if less:
+        print("You're wrong. Your guess was less than the secret number.")
+
+
+print("*" * 29)
+print("End Game")
