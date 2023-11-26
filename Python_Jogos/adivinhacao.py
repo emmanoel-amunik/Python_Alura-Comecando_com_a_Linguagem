e757ad530992609b81a8baf607e4f39e130817af
emmanoel-amunik <emmanoelamunik@hotmail.com>
2023-11-26 07:34:33 -0300

    new adds

--- Python_Jogos/adivinhacao.py
+++ Python_Jogos/adivinhacao.py
@@ -2,26 +2,33 @@ print("*" * 29)
 print("Welcome to the Guessing game!")
 print("*" * 29)
 
-secret_number = 85
-user_kick = int(input("Type a number: "))
 
-# user_kick = int(user_kick)  \\ teacher's solution
-print("You entered: ", user_kick)
+for attempts in range(3):
+
+    secret_number = 85
+
+    print(f"Attempt {attempts+1} of 3")
+    user_kick = int(input("Type a number: "))
+
+    if user_kick < 1 or user_kick > 100:
+        print("You need to enter a number between 1 and 100")
+        continue
+
+    right = user_kick == secret_number
+    greater = user_kick > secret_number
+    less = user_kick < secret_number
+
+    print("You entered: ", user_kick)
+
+    if right:
+        print("You're right!!!")
+        break
+    else:
+        if greater:
+            print("You're wrong. Your guess was greater than the secret number. ")
+        if less:
+            print("You're wrong. Your guess was less than the secret number.")
+    print("*" * 15)
 
 
-right = user_kick == secret_number
-greater = user_kick > secret_number
-less = user_kick < secret_number
-
-
-if right:
-    print("You're right!!!")
-else:
-    if greater:
-        print("You're wrong. Your guess was greater than the secret number. ")
-    if less:
-        print("You're wrong. Your guess was less than the secret number.")
-
-
-print("*" * 29)
 print("End Game")
